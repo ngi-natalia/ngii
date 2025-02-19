4ff5d13b3edb7b46203dc0ca783d45b606191319
ngi-natalia <grodikova.n@icloud.com>
2025-02-19 16:24:27 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,25 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log('Скрипт отработал корректно')
+
+});
+/*Алгоритм:
+* 
+*1.Начало
+*2. Ожидание клика на карточку товара
+*3. Проверка условия: было нажатие на карточку товара
+*   3.1 Да: Карточка открывается на всю страницу
+*       3.1.1 Проверка условия: было ли еще нажатие
+             3.1.1.1 Да: Нажать на картинку для закрытия
+             3.1.1.2 Нет: конец
+    3.2 Нет: Конец
+4. Конец
+*
+*Блок-схема: /images/schema.pdf
+*/
+const images =document.getElementsByClassName("product-image");
+images[0].addEventListener("click", (image) => {
+    console.log('Клик отработал корректно');
+    /*image.style="transform: scale(2)"; */
+
 });
